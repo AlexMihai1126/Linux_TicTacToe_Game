938e6362d8e3f155152357866b54983493732793
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2023-11-29 17:31:26 +0200

    code cleanup

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 void deseneazaTablaJoc(char tablaJoc[3][3]) {
     clear();
 
-    for (int i = 0; i < 3; ++i) {
-        for (int j = 0; j < 3; ++j) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
             printw("%c", tablaJoc[i][j]);
             if (j < 2) {
                 printw(" | ");
